86812b377ca6702a6ed0fc08e7161aa38efaadc3
BChip <bachippi@svsu.edu>
2018-04-11 02:39:30 -0400

    Change Feature Columns

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
     {gridItems.map(item => (
-      <div key={item.image} className="column is-6">
+      <div key={item.image} className="column is-4">
         <section className="section">
           <p className="has-text-centered">
             <img alt="" src={item.image} />
