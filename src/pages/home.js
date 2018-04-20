41e54e03a112e8d87873a53ec0f95f7896c0d064
BChip <bachippi@svsu.edu>
2018-04-19 22:58:48 -0400

    hello world

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+import Helmet from 'react-helmet'
+import graphql from 'graphql'
+
+export default function HomePage () {
+  return (
+    <div>
+      hello world
+    </div>
+  )
+}
+
+
